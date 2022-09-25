379aa8658240ef51395e57eeb1a674487b9bf214
Aerilym <aerilymbiz@gmail.com>
2022-09-25 14:28:38 +1000

    Crease supabase worker

---
+++ src/index.ts
@@ -0,0 +1,64 @@
+import { createClient } from '@supabase/supabase-js';
+
+/**
+ * The bindings assigned to the Worker.
+ * @see {@link https://developers.cloudflare.com/workers/runtime-apis/kv/#referencing-kv-from-workers}
+ * @param NAMESPACE_NAME The KV namespace.
+ * @example
+ * const url = await env.NAMESPACE_NAME.get('URL');
+ */
+export interface Env {
+  NAMESPACE_NAME: KVNamespace;
+}
+
+export default {
+  /**
+   * The fetch handler is called whenever a client makes a request to the worker endpoint.
+   * @see {@link https://developers.cloudflare.com/workers/runtime-apis/fetch-event/#syntax-module-worker}
+   * @param request The incoming HTTP request.
+   * @param env The bindings assigned to the Worker.
+   * @param ctx The context of the Worker.
+   * @returns The response outcome to the request.
+   */
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    return handleFetch({ request, env, ctx });
+  },
+  /**
+   * The scheduled handler is called whenever a scheduled cron event is triggered.
+   * @see {@link https://developers.cloudflare.com/workers/runtime-apis/scheduled-event/#syntax-module-worker}
+   * @param controller The scheduled event information.
+   * @param env The bindings assigned to the Worker.
+   * @param ctx The context of the Worker.
+   */
+  async scheduled(controller: ScheduledController, env: Env, ctx: ExecutionContext): Promise<void> {
+    return handleScheduled({ controller, env, ctx });
+  },
+};
+
+/**
+ * Handle incoming HTTP requests.
+ * @param {FetchPayload} payload The payload containing the request, env and ctx.
+ * @returns The response outcome to the request.
+ */
+async function handleFetch({ request, env, ctx }: FetchPayload): Promise<Response> {
+  const config = {
+    supabaseUrl: env.SB_URL,
+    supabaseKey: env.SB_KEY,
+  };
+  // Provide a custom `fetch` implementation as an option
+  const supabase = createClient(config.supabaseUrl, config.supabaseKey);
+  const body: AuthBody = await request.json();
+  const { user, session, error } = await supabase.auth.signUp({
+    email: body.email,
+    password: body.password,
+  });
+  return new Response(user?.id || error?.message);
+}
+
+/**
+ * Handle incoming scheduled cron events.
+ * @param {ScheduledPayload} payload The payload containing the controller, env and ctx.
+ */
+async function handleScheduled({ controller, env, ctx }: ScheduledPayload): Promise<void> {
+  return;
+}
